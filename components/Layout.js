26903961a828c6af62478a6126ab48ea219e7173
HendelCuartas <hendel_10@hotmail.com>
2020-01-15 11:59:19 -0600

    remove home.js file and set global style on Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,25 @@
 import Header from './Header';
 
 const layoutStyle = {
-  padding: 20,
+  padding: '20px',
   boxSizing: 'border-box',
   width: '80%',
   height: 'auto',
-  margin: '0 auto'
+  margin: '0 auto',
+  background: 'tomato'
 };
 
 const Layout = props => (
   <div style={layoutStyle}>
     <Header />
     {props.children}
+    <style jsx global>{`
+      body {
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+      }
+    `}</style>
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-import Home from './home';
+import Layout from '../components/Layout';
 
-export default function Blog() {
+export default function Home() {
   return (
-    <Home></Home>
+    <Layout>
+      <div class="Home-circle-bg"></div>
+      <h1>Home</h1>
+      <style jsx>
+        {`
+          .Home-circle-bg {
+            position: absolute;
+            width: 120vw;
+            height: 120vw;
+            background: green;
+            top: 0;
+            left: 50%;
+            transform: translate(-50%, -50%);
+          }
+        `}
+      </style>
+    </Layout>
   );
 }
