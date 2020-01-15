3209054b1e0d14e75471a3ccfe62c38f7b6a569b
HendelCuartas <hendel_10@hotmail.com>
2020-01-15 11:38:33 -0600

    remove .next files from github and update index and home

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,7 @@
-import Layout from '../components/Layout';
+import Home from './home';
 
 export default function Blog() {
   return (
-    <Layout>
-      <h1>Home</h1>
-      <style jsx>{`
-        h1,
-        a {
-          font-family: 'Arial';
-        }
-
-        ul {
-          padding: 0;
-        }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
-        }
-      `}</style>
-    </Layout>
+    <Home></Home>
   );
 }
